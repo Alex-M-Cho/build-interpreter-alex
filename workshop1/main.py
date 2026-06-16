3681939e1d23656bbbbd0515fc771138dd1e1307
Alex <alex.m.cho1995@gmail.com>
2026-06-15 21:34:28 -0700

    Update main.py

--- workshop1/main.py
+++ workshop1/main.py
@@ -1,1 +1,10 @@
 print("Hello, World!")
+
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = 5
+    b = 2
+    result = add(a, b)
+    print(f"The result of adding {a} and {b} is: {result}")
